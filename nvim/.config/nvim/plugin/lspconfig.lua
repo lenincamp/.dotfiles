55b337e0b48c50c1a080f6be34c5e148bc8e06c6
lcampoverde <lenincamp18@gmail.com>
2023-01-28 21:56:17 -0500

    update settings

--- nvim/.config/nvim/plugin/lspconfig.lua
+++ nvim/.config/nvim/plugin/lspconfig.lua
@@ -114,6 +114,8 @@ nvim_lsp.emmet_ls.setup({
   }
 })
 
+nvim_lsp.pylsp.setup {}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = true,
