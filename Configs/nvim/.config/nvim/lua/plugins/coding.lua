be9b8fb7dc8d55e17b6757ac27a3c73d81e751c9
lcampoverde <lenincamp18@gmail.com>
2025-03-09 00:53:46 -0500

    add languages to install mason/treesitter

--- Configs/nvim/.config/nvim/lua/plugins/coding.lua
+++ Configs/nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,17 +1,79 @@
 return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "java",
+        "vim",
+        "vimdoc",
+        "regex",
+        "sql",
+        "toml",
+        "xml",
+        "yaml",
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "bash-language-server",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "eslint-lsp",
+        "hadolint",
+        "java-debug-adapter",
+        "java-test",
+        "lua-language-server",
+        "prettier",
+        "shellcheck",
+        "shfmt",
+        "sonarlint-language-server",
+        "sqlfluff",
+        "stylua",
+        "tailwindcss-language-server",
+        "taplo",
+        "typescript-language-server",
+        "yaml-language-server",
+      },
+    },
+  },
   -- Incremental rename
   {
     "smjonas/inc-rename.nvim",
     cmd = "IncRename",
     config = true,
   },
-  {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = { "hrsh7th/cmp-emoji" },
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, { name = "emoji" })
+  --   end,
+  -- },
   {
     "rmagatti/goto-preview",
     event = "BufEnter",
@@ -21,27 +83,35 @@ return {
   {
     "mg979/vim-visual-multi",
   },
-
-  -- Refactoring tool
-  -- {
-  --   "ThePrimeagen/refactoring.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   lazy = false,
-  --   keys = {
-  --     {
-  --       "<leader>r",
-  --       function()
-  --         require("refactoring").select_refactor()
-  --       end,
-  --       mode = "v",
-  --       noremap = true,
-  --       silent = true,
-  --       expr = false,
-  --     },
-  --   },
-  --   opts = {},
-  -- },
+  {
+    "danarth/sonarlint.nvim",
+    lazy = true,
+    ft = { "java" },
+    opts = {
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          -- Ensure that sonarlint-language-server uses stdio channel
+          "-stdio",
+          "-analyzers",
+          -- paths to the analyzers you need, using those for python and java in this example
+          -- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+          -- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+        },
+        -- All settings are optional
+        settings = {
+          -- The default for sonarlint is {}, this is just an example
+          sonarlint = {},
+        },
+      },
+      filetypes = {
+        -- Tested and working
+        -- "python",
+        -- "cpp",
+        -- Requires nvim-jdtls, otherwise an error message will be printed
+        "java",
+      },
+    },
+  },
 }
