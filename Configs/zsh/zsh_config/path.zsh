a0e945eb7dda5cc8ae871253a12e4168dcd36fe2
lcampoverde <lenincamp18@gmail.com>
2025-12-08 11:40:23 -0500

    fzf completion menu

--- Configs/zsh/zsh_config/path.zsh
+++ Configs/zsh/zsh_config/path.zsh
@@ -35,7 +35,7 @@ export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
 # Completion styling
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*' menu no
+zstyle ':completion:*' menu select
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
 zstyle ':completion:*:git-checkout:*' sort false
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
