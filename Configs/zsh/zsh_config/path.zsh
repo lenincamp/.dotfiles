09dbf2b628532f38027c2c25c3aa6c9ce432ccd0
lcampoverde <lenincamp18@gmail.com>
2025-12-08 10:55:51 -0500

    update salesforce tab text and new salesforce functions

--- Configs/zsh/zsh_config/path.zsh
+++ Configs/zsh/zsh_config/path.zsh
@@ -25,7 +25,6 @@ export VISUAL=$EDITOR
 
 ######### fzf #######
 export FZF_DEFAULT_COMMAND='fd --type f --color=never --hidden'
-export FZF_DEFAULT_OPTS="--layout=reverse --no-height --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --color=border:#313244,label:#cdd6f4"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}' --preview-window=up:85%:nowrap --layout=reverse --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,ctrl-k:preview-up,ctrl-j:preview-down,ctrl-n:down,ctrl-p:up'"
 if [[ $FIND_IT_FASTER_ACTIVE -eq 1 ]]; then
