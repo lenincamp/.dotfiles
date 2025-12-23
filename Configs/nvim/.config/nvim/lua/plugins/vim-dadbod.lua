33f44fa951dca042a76414e03d803f29361314ed
lcampoverde <lenincamp18@gmail.com>
2025-12-23 08:59:44 -0500

    feat(nvim): prune plugins and add gitsigns
    - Replace git-blame.nvim with gitsigns.nvim for blame and hunk signs
    - Remove vim-dadbod and related UI/completion dependencies
    - Drop LazyVim extras: editor.harpoon2, editor.mini-diff, lang.rust
    - Remove crates.nvim, harpoon, mini.diff, rustaceanvim from lock
    - Deduplicate vscode extra in lazyvim.json
    - Bump locks for mason-lspconfig, nvim-dap, nvim-lint, lspconfig,
      treesitter, and others
    
    BREAKING CHANGE: Rust tooling (LazyVim rust extra) and database tooling
    (dadbod) were removed. Re-enable the rust extra and reinstall dadbod if
    you still need them.

--- Configs/nvim/.config/nvim/lua/plugins/vim-dadbod.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "kristijanhusak/vim-dadbod",
-  lazy = true,
-  optional = true,
-  branch = "async-query",
-  dependencies = { "kristijanhusak/vim-dadbod-ui", "kristijanhusak/vim-dadbod-completion" },
-}
